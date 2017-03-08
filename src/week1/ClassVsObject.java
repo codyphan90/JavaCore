bcdc662d2324561b8368f5fddb077dfad8fd497b
phanvietanh <phanvietanh90@gmail.com>
2017-03-08 12:49:05 +0700

    enum example

--- src/week1/ClassVsObject.java
+++ src/week1/ClassVsObject.java
@@ -5,6 +5,23 @@ public class ClassVsObject {
 	
 //  Class is set of properties and methods which don't change during the execution of program, it's general concept
 //	Object is created and can be destroyed, belong to a Class, property can be change,
+	
+	 public static int x = 7; 
+	    public int y = 3; 
+	    
+	    public static void main(String[] args){
+	    	ClassVsObject a =  new ClassVsObject();
+	    	ClassVsObject b =  new ClassVsObject();
+	    	a.y = 5;
+	    	b.y = 6;
+	    	a.x = 1;
+	    	b.x = 2;
+	    	System.out.println("a.y = " + a.y); //5
+	    	System.out.println("b.y = " + b.y); //6
+	    	System.out.println("a.x = " + a.x); //2
+	    	System.out.println("b.x = " + b.x); // 2
+	    	System.out.println("ClassVsObject.x = " + ClassVsObject.x); //2
+	    }
 }
 
 
--- src/week1/EnumType.java
+++ src/week1/EnumType.java
@@ -1,17 +1,72 @@
 package week1;
 
-public enum WeekDay {
-	   MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+public class EnumType {
+	public enum COLOUR{
+		RED, BLUE, YELLOW
+	};
+	
+	public enum Gender {
+		MALE("M","MALE"),FEMALE("F","FEMALE");
+		private String code;
+		private String text;
+		private Gender(String code, String text) {
+			this.code = code;
+			this.text = text;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getText() {
+			return text;
+		}
+		public void setText(String text) {
+			this.text = text;
+		}
+		
+	public static Gender getGenderbyCode(String code){
+		for(Gender gender: Gender.values()){
+			if (gender.code.equals(code)){
+				return gender;
+			}
+		}
+		return null;
 	}
-public class Timetable {
-	 
-	  public static String getJob(WeekDay weekDay) {
-	      if (weekDay == WeekDay.SATURDAY || weekDay == WeekDay.SUNDAY) {
-	          return "Nothing";
-	      }
-	      return "Coding";
-	  }
-	 
+		
 	}
-
-
+	public static void main(String[] args){
+		
+		//get by value
+		Gender marryGender = Gender.FEMALE;
+		System.out.println("Code: " + marryGender.getCode());
+		System.out.println("Text: " + marryGender.getText());
+		System.out.println("Ordinal: "+ marryGender.ordinal());
+		
+		//list all
+		System.out.println("Values of Gender:");
+		for (Gender gender : Gender.values()){
+			System.out.println(gender.getText());
+		}
+		
+		//get by code
+		String code = "M";
+		Gender gender = Gender.getGenderbyCode(code);
+		System.out.println("Gender by code: " + gender);
+		
+		//Example 2: Colour
+		System.out.println(COLOUR.BLUE);
+		System.out.println(COLOUR.RED);
+		COLOUR c = COLOUR.RED;
+		System.out.println(c);
+		System.out.println(c.YELLOW);
+		if (c == COLOUR.YELLOW){
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+	
+	
+}
